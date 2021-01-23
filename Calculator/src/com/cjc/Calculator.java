aef1fc8fe4d690f13dd7b0b835a67b31d4c010f2
tushar-nawale <tusharnawale344@gmail.com>
2021-01-23 12:14:33 +0530

    calculator commit

---
+++ Calculator/src/com/cjc/Calculator.java
@@ -0,0 +1,23 @@
+package com.cjc;
+
+public class Calculator {
+public void add()
+{
+	System.out.println("Addition");
+	
+}
+public void sub()
+{
+	System.out.println("Substract");
+}
+public void mul()
+{
+	System.out.println("Multiplication");
+	
+}
+public void div()
+{
+	System.out.println("Division");
+}
+
+}
---
+++ Calculator/src/com/cjc/Test.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class Test {
+public static void main(String args[])
+{
+	Calculator obj=new Calculator();
+			   obj.add();
+			   obj.sub();
+			   obj.mul();
+			   obj.div();
+}
+}
